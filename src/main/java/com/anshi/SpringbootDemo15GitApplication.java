66b85c58480eb552e7e0f3db5358f5a33b5263a8
hsy <hhssyy@163.com>
2022-10-30 17:26:40 +0800

    1.1

---
+++ src/main/java/com/anshi/SpringbootDemo15GitApplication.java
@@ -0,0 +1,13 @@
+package com.anshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo15GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo15GitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anshi/SpringbootDemo15GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo15GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
